563f4b37561959266f02fad0534d5d9c001d757f
IanB28 <german.buzzo12@gmail.com>
2025-03-22 13:53:57 -0600

    cookies

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,12 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, Res } from '@nestjs/common';
+import { Response } from 'express';
 import { AuthService } from './auth.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { LoginUserDto } from './dto/login-user.dto';
 import { ApiAuth } from './decorators/api.decorator';
+import { TOKEN_NAME } from './constants/jwt.constants';
+import { Cookies } from './decorators/cookies.decorator';
 
 
 @ApiAuth()
@@ -16,9 +19,11 @@ export class AuthController {
    return this.authService.registerUser(CreateAuthDto)
   }
   @Post("login")
-  login(@Body() loginUserDto :LoginUserDto){
-    console.log(loginUserDto);
-   return this.authService.loginUser(loginUserDto)
+  async login(@Body() loginUserDto :LoginUserDto, @Res({passthrough: true}) response: Response, @Cookies() cookies: any) {
+  const token = await this.authService.loginUser(loginUserDto)
+  console.log('token', token);
+   response.cookie(TOKEN_NAME, token, { httpOnly: false, secure: true, sameSite:'none' ,maxAge: 1000 * 60 * 60 * 24 * 7 })
+   return;
   }
   @Patch("/:email")
   updateUser(@Param('email') userEmail, @Body() updateUserDto: UpdateUserDto ){
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -20,21 +20,21 @@ export class AuthService {
           return this.userRepository.save(createUserDto); 
   }  
   async loginUser(loginUserDto: LoginUserDto) {
-    console.log(loginUserDto)
+   
     const user = await this.userRepository.findOne({
       where:{
       userEmail: loginUserDto.userEmail,
       },
     })
-console.log(user);
+
     if (!user) throw new UnauthorizedException("Usuario NO encontrado");
   
     const match = await bcrypt.compare
     (loginUserDto.userPassword,
        user.userPassword,
-    )
+    );
     //src/auth/auth.service.ts:23:62 - error TS18047: 'user' is possibly 'null' por eso agregue el otro if:)
-    console.log(match);
+
         if(!match) throw new UnauthorizedException("NO esta autorizado");
         const payload = {
           userEmail: user.userEmail, 
